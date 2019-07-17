c1e56dc5d4798b19988931019ab8019b50d7090c
imad-ramy <turlax800@outlook.fr>
2019-07-17 14:38:57 +0100

    first commit

---
+++ src/ListItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Item from "./item";
+
+
+function ListItem(){
+    return <>
+        <Item/>
+        <Item/>
+        <Item/>
+     
+    </>
+}
+export default ListItem;
---
+++ src/item.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Item(){
+    return ( 
+    <div>
+        <li> <a>Home</a> </li>
+        <li> <a>About</a> </li>
+        <li> <a>Contact</a> </li>
+    </div>
+    )
+}
+export default Item;
+
